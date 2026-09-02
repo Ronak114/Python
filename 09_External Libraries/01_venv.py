63eeeaee7154d22143fa23307ed572849a78652a
Ronak114 <pawarronak2003@gmail.com>
2026-09-02 19:16:08 +0530

    Requests

--- 09_External Libraries/01_venv.py
+++ 09_External Libraries/01_venv.py
@@ -1,5 +1,6 @@
 # to install : pip install packageName
 
+
 # Virtual Environment (venv): is a tool to create isolated Python environments. It allows you to manage dependencies for different projects separately.
 
 # virtual env solves the problem of dependency conflicts between different projects. It allows you to create a self-contained environment for each project, with its own set of packages and dependencies.
@@ -12,3 +13,13 @@
 
 # Requirements file: A requirements file is a text file that lists all the dependencies required for a Python project.
 # pip freeze > requirements.txt
+
+
+# Requests library: Python library for making HTTP requests. It provides a simple and intuitive API for sending HTTP requests and handling responses.
+
+import requests
+
+r = requests.get("https://www.google.com")
+
+with open("code.txt", "w") as f:
+    f.write(r.text)
