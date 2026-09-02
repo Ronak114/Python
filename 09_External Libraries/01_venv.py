f19b0d4f4702f7756d08b0ee7fc9291cc2be555d
Ronak114 <pawarronak2003@gmail.com>
2026-09-02 18:52:10 +0530

    virtual env

---
+++ 09_External Libraries/01_venv.py
@@ -0,0 +1,14 @@
+# to install : pip install packageName
+
+# Virtual Environment (venv): is a tool to create isolated Python environments. It allows you to manage dependencies for different projects separately.
+
+# virtual env solves the problem of dependency conflicts between different projects. It allows you to create a self-contained environment for each project, with its own set of packages and dependencies.
+
+# install - pip install virtualenv
+# create - python -m venv env_name
+# activate - .\env_name\Scripts\Activate.ps1"
+# deactivate - deactivate.
+
+
+# Requirements file: A requirements file is a text file that lists all the dependencies required for a Python project.
+# pip freeze > requirements.txt
